0a0a235f3944d84a9f12f1618b557b9b713e64ac
ADITYAA AANAND <aditya777anand@gmail.com>
2026-03-19 09:06:45 +0530

    Remove commented code in Note.jsx

--- src/components/Windows/Note.jsx
+++ src/components/Windows/Note.jsx
@@ -3,9 +3,8 @@ import Markdown from "react-markdown";
 import MacWindow from "./MacWindow";
 import { atelierDuneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import "./note.scss";
-//react-syntax-highlighter
+
 import SyntaxHighlighter from "react-syntax-highlighter";
-//this code in note.txt is in markdown format to render in react install package npm i react-markdown
 
 const Note = ({windowName, setWindowsState}) => {
   const [markdown, setmarkdown] = useState(null);
